08dcff8396124f03e2067f6e2158c04d3cbd2608
initMoin <init.moin@gmail.com>
2026-08-14 17:06:59 -0500

    Initial Commit

---
+++ ContinuoTests/ContinuoTests.swift
@@ -0,0 +1,18 @@
+//
+//  ContinuoTests.swift
+//  ContinuoTests
+//
+//  Created by Moinuddin Ahmad on 8/14/26.
+//
+
+import Testing
+
+struct ContinuoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
