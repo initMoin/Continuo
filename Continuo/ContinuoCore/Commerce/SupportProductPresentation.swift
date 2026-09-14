c7515c583dfde0209806a996367ed0ed20bf12cf
initMoin <init.moin@gmail.com>
2026-09-14 04:57:15 -0500

    Keep support product copy with commerce types

--- Continuo/ContinuoCore/Commerce/SupportProductPresentation.swift
+++ Continuo/ContinuoCore/Commerce/SupportProductPresentation.swift
@@ -1,15 +1,4 @@
 extension SupportProduct {
-    var message: String {
-        switch self {
-        case .lemonCookie:
-            "A small bite of thanks."
-        case .caramelLatte:
-            "For the builds that run past bedtime."
-        case .phillyCheesesteak:
-            "A hearty thank-you for keeping Continuo going."
-        }
-    }
-
     var assetName: String {
         switch self {
         case .lemonCookie:
